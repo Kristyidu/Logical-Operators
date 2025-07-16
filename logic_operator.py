9652caf165eade7f972df0846f34fbe5a446735e
kristyidu <xtyidu@yahoo.com>
2025-07-16 13:08:51 +0100

    first commit

---
+++ logic_operator.py
@@ -0,0 +1,61 @@
+#A simple calculator app
+print("welcome to our simple calculator app")
+print('''****************
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+**********************'''
+)
+print ("Enter two numbers to add")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+#prompt the user for number and store it 
+second_number = input("Second Number:")
+sum = float(first_number) + float(second_number)
+print(f"{first_number} + {second_number} = {sum}")
+
+print("**********************")
+print("subtraction")
+
+print ("Enter two numbers to subtract")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+#prompt the user for number and store it 
+second_number = input("Second Number:")
+sub = float(first_number) - float(second_number)
+print(f"{first_number} - {second_number} = {sub}")
+
+print("**********************")
+print("Multiplication")
+
+print ("Enter two numbers to multiply")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+#prompt the user for number and store it 
+second_number = input("Second Number:")
+mul = float(first_number) * float(second_number)
+print(f"{first_number} * {second_number} = {mul}")
+
+print("**********************")
+print("Division")
+
+print ("Enter two numbers to divide")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+#prompt the user for number and store it 
+second_number = input("Second Number:")
+div = float(first_number) / float(second_number)
+print(f"{first_number} / {second_number} = {div}")
+
+print("**********************")
+print("Exponential")
+
+print ("Enter two numbers to find the exponentials")
+#prompt a user for a number and store it
+first_number = input("First Number:")
+#prompt the user for number and store it 
+second_number = input("Second Number:")
+exp = float(first_number) ** float(second_number)
+print(f"{first_number} ** {second_number} = {exp}")
+
